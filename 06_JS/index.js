0618729386fbc907a800507ae613d275bf0dc424
Ankita Arya <ankitaarya1207@gmail.com>
2025-05-16 20:19:06 +0530

    datatypes of js

---
+++ 06_JS/index.js
@@ -0,0 +1,34 @@
+// Primitive datatypes
+// 7 types: string, Number, Boolean, Null, undefined,Symbol(unique), BigInt
+
+const score=100
+const scoreValue=100.3
+const isLoggedIn = false
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId = Symbol('123')
+console.log(id==anotherId);
+
+const bigNumber=17389392n
+
+
+
+
+
+//Reference (Non Primitives)
+//Array, Objects, Functions
+
+const heroes=["shaktimaan", "naagraj","doga"]
+let myObj={
+    name:"ankita",
+    age:23,
+}
+
+const myFunction=function(){
+    console.log("hello world");
+    
+}
+
+
