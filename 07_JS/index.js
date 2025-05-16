687cf08d09add647c72912b120b954d85a938b55
Ankita Arya <ankitaarya1207@gmail.com>
2025-05-16 21:57:04 +0530

    stack and heap

---
+++ 07_JS/index.js
@@ -0,0 +1,17 @@
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "ankita123"
+let anothername = myYoutubename
+anothername="chai and code"
+console.log(myYoutubename)
+console.log(anothername);
+
+let userOne = {
+    email: "ankita@google.com",
+    upi: "user@ybi"
+}
+
+let userTwo = userOne
+userTwo.email = "hitesh@google.com"
+console.log(userOne.email)
+console.log(userTwo.email)
