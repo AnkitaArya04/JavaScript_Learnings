6845749946044d185d7d4bae62e5347b6c9bb06f
Ankita Arya <ankitaarya1207@gmail.com>
2025-05-16 13:09:56 +0530

    Datatyes and ECMA standard

---
+++ 02_JS/index.js
@@ -0,0 +1,26 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs not browser
+
+console.log(3+3);
+console.log("Ankita");
+
+let name="Prince"
+let age=12;
+let isLoggedIn = false
+
+// number => 2 to power 53
+// string => ""
+// boolean => true/false
+//bigInt
+// null : standalone value
+// undefined => 
+// Symbol => unique
+
+// Object 
+console.log(typeof undefined);   // Undefined
+console.log(typeof null);  //Object
+
+
+
+
