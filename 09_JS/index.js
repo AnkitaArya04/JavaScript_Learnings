073b6295f8bb4e41d4eb8a5abfff511fdea5f005
Ankita Arya <ankitaarya1207@gmail.com>
2025-05-17 19:08:28 +0530

    string in js

---
+++ 09_JS/index.js
@@ -0,0 +1,29 @@
+const name="Ankita"
+const repoCount=34
+
+console.log(name + repoCount + "value")
+
+const gameName=new String("ankita-hc-com")
+console.log(gameName[0])
+console.log(gameName.__proto__)
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('t'))
+
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString)
+
+
+const newStringOne="  ankita  "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "https://ankita.com/ankita%20choudhary"
+console.log(url.replace('%20','-'))
+console.log(url.includes('sundar'))
+console.log(gameName.split('-'))
