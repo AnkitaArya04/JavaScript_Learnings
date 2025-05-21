ec9716dc0bb6524a7842f599ad39ee4c4b209ce6
Ankita Arya <ankitaarya1207@gmail.com>
2025-05-21 21:18:15 +0530

    Object de structuring

---
+++ 17_JS/index.js
@@ -0,0 +1,16 @@
+const course = {
+    coursename : "js in hinde",
+    price : "999",
+    courseInstructor : "ankita"
+}
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+// console.log(instructor)
+
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(company = "ankita")
+
