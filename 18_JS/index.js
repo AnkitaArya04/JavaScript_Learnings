447d25f887c4b201a9133d2afe452dd66a37d215
Ankita Arya <ankitaarya1207@gmail.com>
2025-05-21 21:50:26 +0530

    functions and scope

---
+++ 18_JS/index.js
@@ -0,0 +1,60 @@
+function sayMyName(){
+    console.log("A")
+    console.log("N")
+    console.log("K")
+    console.log("I")
+    console.log("T")
+    console.log("A")
+}
+// sayMyName()
+
+// function addTwoNumber(number1, number2){
+
+//     console.log(number1 + number2)
+// }
+
+function addTwoNumber(number1,number2){
+    let result=number1+number2
+    return result
+}
+const result=addTwoNumber(2,3)
+// console.log(result)
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("Please enter a username")
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("ankita"))
+
+function calulateCartPrice(val1, val2, ...num1){
+    return num1
+}
+console.log(calulateCartPrice(200,400,500,2000))
+
+const user = {
+    username: "ankita",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+// handleObject(user)
+handleObject({
+    username:"sam",
+    price:399
+})
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200,400,500,1000]))
