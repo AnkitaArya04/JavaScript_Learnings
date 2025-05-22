9ed279cb6d89710e6494623745decfd6170ff283
Ankita Arya <ankitaarya1207@gmail.com>
2025-05-22 20:07:58 +0530

    scope and mini hoisting

---
+++ 21_JS/index.js
@@ -0,0 +1,34 @@
+function one(){
+    const username = "ankita"
+
+    function two(){
+        const website  = "youtube"
+        console.log(username)
+    }
+    // console.log(website)
+    two()
+}
+
+// one()
+
+if(true){
+    const username="ankita"
+    if(username === "ankita"){
+        const website="youtube"
+        // console.log(username + website)
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+// +++++++++++++++++++ interesting
+console.log(addOne(5))
+function addOne(num){
+    return num+1;
+}
+
+
+const addTwo = function(num){
+    return num+2;
+}
+console.log(addTwo(5))
