a2246770019fcf1adfc634f4ec613828843038b4
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-02 23:39:38 +0530

    Js logged in

--- 19_JS/index.js
+++ 19_JS/index.js
@@ -1,26 +1,26 @@
-function calculateCartPrice(val1,val2,...num1){
-    return num1;
+// function calculateCartPrice(val1,val2,...num1){
+//     return num1;
 
-}
-// console.log(calculateCartPrice(200,400,500))
+// }
+// // console.log(calculateCartPrice(200,400,500))
 
-const user={
-    username: "ankita",
-    price :199
-}
+// const user={
+//     username: "ankita",
+//     price :199
+// }
 
-function handleObject(anyObject){
-    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
-}
-// console.log(handleObject(user))
-handleObject({
-    username:"sam",
-    price:3829
-})
+// function handleObject(anyObject){
+//     console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+// }
+// // console.log(handleObject(user))
+// handleObject({
+//     username:"sam",
+//     price:3829
+// })
 
-const newArray=[200,300,292,3929]
-function returnSecondValue(getArray){
-    return getArray[1]
-}
-// console.log(returnSecondValue(newArray))
-console.log(returnSecondValue([200,400,2829]))
+// const newArray=[200,300,292,3929]
+// function returnSecondValue(getArray){
+//     return getArray[1]
+// }
+// // console.log(returnSecondValue(newArray))
+// console.log(returnSecondValue([200,400,2829]))
