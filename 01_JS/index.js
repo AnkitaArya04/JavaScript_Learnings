3db78ea69e8cd101782d5a151a7ff747cb3819af
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-02 23:35:13 +0530

    Js

--- 01_JS/index.js
+++ 01_JS/index.js
@@ -1,13 +1,13 @@
-const accountId = 144552
-let accountEmail = "ankitarya@gmail.com"
-var accountPassword = "12345"
-accountCity = "Jaipur"
-let accountState
+// const accountId = 144552
+// let accountEmail = "ankitarya@gmail.com"
+// var accountPassword = "12345"
+// accountCity = "Jaipur"
+// let accountState
 
-// accountId=2  //not allowed
-accountEmail="parsad@gmail.com"
-accountPassword="4658"
-accountCity="Bhilai"
-console.log(accountId);
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+// // accountId=2  //not allowed
+// accountEmail="parsad@gmail.com"
+// accountPassword="4658"
+// accountCity="Bhilai"
+// console.log(accountId);
+// console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
 
