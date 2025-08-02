1d54e6fe91cb2d1a30857501a7b0caa70a2b1c87
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-02 23:36:36 +0530

    Js logged in

--- 02_JS/index.js
+++ 02_JS/index.js
@@ -1,25 +1,25 @@
-// "use strict" // treat all JS code as newer version
+"use strict" // treat all JS code as newer version
 
-// // alert(3+3) //we are using nodejs not browser
+// alert(3+3) //we are using nodejs not browser
 
-// console.log(3+3);
-// console.log("Ankita");
+console.log(3+3);
+console.log("Ankita");
 
-// let name="Prince"
-// let age=12;
-// let isLoggedIn = false
+let name="Prince"
+let age=12;
+let isLoggedIn = false
 
-// // number => 2 to power 53
-// // string => ""
-// // boolean => true/false
-// //bigInt
-// // null : standalone value
-// // undefined => 
-// // Symbol => unique
+// number => 2 to power 53
+// string => ""
+// boolean => true/false
+//bigInt
+// null : standalone value
+// undefined => 
+// Symbol => unique
 
-// // Object 
-// console.log(typeof undefined);   // Undefined
-// console.log(typeof null);  //Object
+// Object 
+console.log(typeof undefined);   // Undefined
+console.log(typeof null);  //Object
 
 
 
