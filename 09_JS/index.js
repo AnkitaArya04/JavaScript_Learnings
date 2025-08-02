33696fad770d405a9f574b6f16b0188733b5773d
Ankita Arya <ankitaarya1207@gmail.com>
2025-08-02 23:38:02 +0530

    Js logged in

--- 09_JS/index.js
+++ 09_JS/index.js
@@ -1,29 +1,29 @@
-const name="Ankita"
-const repoCount=34
+// const name="Ankita"
+// const repoCount=34
 
-console.log(name + repoCount + "value")
+// console.log(name + repoCount + "value")
 
-const gameName=new String("ankita-hc-com")
-console.log(gameName[0])
-console.log(gameName.__proto__)
+// const gameName=new String("ankita-hc-com")
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
 
-console.log(gameName.length)
-console.log(gameName.toUpperCase())
-console.log(gameName.charAt(2))
-console.log(gameName.indexOf('t'))
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('t'))
 
-const newString = gameName.substring(0,4)
-console.log(newString)
+// const newString = gameName.substring(0,4)
+// console.log(newString)
 
-const anotherString = gameName.slice(-8,4)
-console.log(anotherString)
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString)
 
 
-const newStringOne="  ankita  "
-console.log(newStringOne)
-console.log(newStringOne.trim())
+// const newStringOne="  ankita  "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
 
-const url = "https://ankita.com/ankita%20choudhary"
-console.log(url.replace('%20','-'))
-console.log(url.includes('sundar'))
-console.log(gameName.split('-'))
+// const url = "https://ankita.com/ankita%20choudhary"
+// console.log(url.replace('%20','-'))
+// console.log(url.includes('sundar'))
+// console.log(gameName.split('-'))
